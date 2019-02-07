5456aa56558a9559cec4ec0b51211a6537110067
Co <coaction2@gmail.com>
2019-02-07 01:40:33 -0800

    Apples and Oranges. Kangaroo Counting Completed.

--- HackerRankProblems/Program.cs
+++ HackerRankProblems/Program.cs
@@ -7,8 +7,67 @@ namespace HackerRankProblems
 {
     class Program
     {
+        // Apples and Oranges Hacker Rank
+        static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+        {
+            int HouseLeft = s;
+            int HouseRight = t;
+            int AppleTreeLocation = a;
+            int OrangeTreeLocation = b;
+            int AppleCount = 0;
+            int OrangeCount = 0;
+            int[] AppleOutAry = new int[apples.Length];
+            int[] OrangesOutAry = new int[oranges.Length];
+            for (int i = 0; i < apples.Length; i++)
+            {
+                AppleOutAry[i] = AppleTreeLocation + apples[i];
+                if (AppleOutAry[i] >= HouseLeft && AppleOutAry[i] <= HouseRight)               
+                    AppleCount++;              
+            }
+            for (int i = 0; i < oranges.Length; i++)
+            {
+                OrangesOutAry[i] = OrangeTreeLocation + oranges[i];
+                if (OrangesOutAry[i] >= HouseLeft && OrangesOutAry[i] <= HouseRight)                
+                    OrangeCount++;
+            }
+            Console.WriteLine(AppleCount);
+            Console.WriteLine(OrangeCount);
+        }
+
+        static string kangaroo(int x1, int v1, int x2, int v2)
+        {
+            int count = 0;
+            int x = 0;
+            int v = 0;
+            x += x2;
+            v += x1;
+            while (count <= 10000)
+            {
+                x += v2;
+                v += v1;
+                ++count;
+
+                if (x == v)
+                {
+                    return "YES";
+                }
+            }
+
+            return "NO";
+
+        }
+
+
         static void Main(string[] args)
         {
+            // Apples And Oranges Test Input Hacker Rank
+            //int[] Apple = new int[] { 2, 3, -4 };
+            //int[] Orange = new int[] { 3, -2, -4 };
+            //countApplesAndOranges(7, 10, 4, 12, Apple, Orange);
+
+            Console.WriteLine(kangaroo(0, 3, 4, 2));
+            //Console.WriteLine(kangaroo(2, 1, 1, 2));
+
         }
     }
 }
